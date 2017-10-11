f6379471ee5c981a30206ad1ae23561e8cc910fd
wertrain <wertrain@gmail.com>
2017-10-11 23:53:55 +0900

    コメントの追加

--- GameFramework/Framework/Common/Defines.h
+++ GameFramework/Framework/Common/Defines.h
@@ -1,6 +1,12 @@
 #ifndef DEFINES_H_
 #define DEFINES_H_
 
+/**
+ * DirectX の使用を明示する定義
+ * Types.h で DirectX の vector や matrix のラップを判定するのに使っているが
+ * そもそも、フレームワークでは Device などを、生のまま使用しているので、DirectXを使用する前提のコードになっている。
+ * 気が向けば、そのあたりもラップするかもしれないが、今のところはこの定義はあってないようなもの。
+ */ 
 #define FRAMEWORK_USE_DIRECT_X_
 
 #endif // DEFINES_H_
