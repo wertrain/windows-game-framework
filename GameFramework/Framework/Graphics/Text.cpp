613fd97bbd48fa0d6419fbc03868c421225c7add
wertrain <wertrain@gmail.com>
2017-10-20 00:34:00 +0900

    テキスト出力準備

---
+++ GameFramework/Framework/Graphics/Text.cpp
@@ -0,0 +1,18 @@
+#include "Text.h"
+
+namespace Framework {
+namespace Graphics {
+
+Text::Text()
+    : GraphicsBase()
+{
+
+}
+
+Text::~Text()
+{
+
+}
+
+} // namespace Graphics
+} // namespace Framework
---
+++ GameFramework/Framework/Graphics/Text.h
@@ -0,0 +1,19 @@
+#ifndef TEXT_H_
+#define TRIANGLE_H_
+
+#include "GraphicsBase.h"
+
+namespace Framework {
+namespace Graphics {
+
+class Text : public GraphicsBase
+{
+public:
+    Text() ;
+    virtual ~Text();
+};
+
+} // namespace Graphics
+} // namespace Framework
+
+#endif // TRIANGLE_H_
