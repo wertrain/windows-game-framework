a6e2d08e64643386fbe5cb44b639119e3992dc5b
wertrain <wertrain@gmail.com>
2017-10-21 18:23:38 +0900

    Render2Dを追加

---
+++ GameFramework/Framework/Graphics/Render2D.cpp
@@ -0,0 +1,17 @@
+#include "Render2D.h"
+
+namespace Framework {
+namespace Graphics {
+
+Render2D::Render2D()
+{
+
+}
+
+Render2D::~Render2D()
+{
+
+}
+
+} // namespace Graphics
+} // namespace Framework
---
+++ GameFramework/Framework/Graphics/Render2D.h
@@ -0,0 +1,17 @@
+#ifndef RENDER2D_H_
+#define RENDER2D_H_
+
+namespace Framework {
+namespace Graphics {
+
+class Render2D
+{
+public:
+    Render2D() ;
+    virtual ~Render2D();
+};
+
+} // namespace Graphics 
+} // namespace Framework
+
+#endif // RENDER2D_H_
