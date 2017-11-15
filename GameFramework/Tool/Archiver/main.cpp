85cc77a1b208813c0df86150e34d95bcfa2fe152
wertrain <wertrain@gmail.com>
2017-11-16 01:07:34 +0900

    引数の変更

--- GameFramework/Tool/Archiver/main.cpp
+++ GameFramework/Tool/Archiver/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "Includes.h"
 
-int main(char* args[])
+int main(const int argc, const char*argv[])
 {
     std::cout << "Usage:" << std::endl;
+    std::cout << argc << std::endl;
 
     return 0;
 }
