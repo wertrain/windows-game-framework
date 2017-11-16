bf90fe5c7b6b2f36405a3cd33ce0e45e71f345fa
wertrain <wertrain@gmail.com>
2017-11-17 00:29:14 +0900

    Usage の記載

--- GameFramework/Tool/Archiver/main.cpp
+++ GameFramework/Tool/Archiver/main.cpp
@@ -4,7 +4,9 @@
 int main(const int argc, const char*argv[])
 {
     std::cout << "Usage:" << std::endl;
-    std::cout << argc << std::endl;
-
+    std::cout << "    Archiver.exe [-a] [-e] <file> ..." << std::endl;
+    std::cout << "Options:" << std::endl;
+    std::cout << "    -a   指定されたファイルを圧縮する" << std::endl;
+    std::cout << "    -e   指定されたファイルを展開する" << std::endl;
     return 0;
 }
