28161f6666a57899e67f5d15e4d0206ed80eb363
wertrain <wertrain@gmail.com>
2017-11-25 11:18:42 +0900

    メモリからテクスチャの作成機能を追加

--- GameFramework/Framework/Graphics/Texture.cpp
+++ GameFramework/Framework/Graphics/Texture.cpp
@@ -38,11 +38,34 @@ Texture::~Texture()
 
 bool Texture::CreateFromFile(ID3D11Device* device, const wchar_t* filename)
 {
-    Framework::System::File::Binary textureFile;
-    if (!textureFile.Read(filename))
-        return false;
-
     HRESULT hr = DirectX::CreateWICTextureFromFile(device, filename, &mTexture, &mShaderResView);
+    if (FAILED(hr))
+    {
+        return false;
+    }
+
+    // ƒTƒ“ƒvƒ‰[ì¬
+    D3D11_SAMPLER_DESC sampDesc;
+    ZeroMemory(&sampDesc, sizeof(sampDesc));
+    sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+    sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+    sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+    sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+    sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+    sampDesc.MinLOD = 0;
+    sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
+    hr = device->CreateSamplerState(&sampDesc, &mSampler);
+    if (FAILED(hr))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool Texture::CreateFromMemory(ID3D11Device* device, const void* buffer, const size_t buffersize)
+{
+    HRESULT hr = DirectX::CreateWICTextureFromMemory(device, (const uint8_t*)buffer, buffersize, &mTexture, &mShaderResView, 0);
     if (FAILED(hr))
     {
         return false;
--- GameFramework/Framework/Graphics/Texture.h
+++ GameFramework/Framework/Graphics/Texture.h
@@ -16,6 +16,7 @@ public:
     Texture() ;
     virtual ~Texture();
     bool CreateFromFile(ID3D11Device* device, const wchar_t* filename);
+    bool CreateFromMemory(ID3D11Device* device, const void* buffer, const size_t buffersize);
     bool Create(ID3D11Device* device, ID3D11Resource* texture);
     const ID3D11Resource* GetTexture();
     const ID3D11SamplerState* GetSamplerState();
