b1798b8868d461c51a50a628a5b893e45abd6718
wertrain <wertrain@gmail.com>
2017-11-25 11:19:37 +0900

    アーカイバソースの追加とテストの追加

--- GameFramework/GameMain.cpp
+++ GameFramework/GameMain.cpp
@@ -3,10 +3,40 @@
 #include "Framework/Graphics/Text.h"
 #include "Framework/Graphics/Texture.h"
 #include "Framework/Graphics/Render2D.h"
+#include "Tool/Archiver/Includes.h"
 
 Framework::Graphics::Text s_Text;
 Framework::Graphics::Texture s_Texure;
 Framework::Graphics::Render2D s_Render2D;
+
+#define ARCHIVER_TEST
+//#define TEXTWRITE_TEST
+
+#ifdef ARCHIVER_TEST
+void ArchiverTest(ID3D11Device* device)
+{
+    Framework::System::File::Binary file;
+    file.Read(L"bintest.bin");
+
+    Framework::Tool::Archiver archiver;
+    archiver.ReadBinary(file.Get(), file.Size());
+
+    const s32 index = 0;
+    const size_t size = archiver.GetDataSize(index);
+    std::unique_ptr<char> data(new char[size]);
+    archiver.GetData(index, data.get());
+
+    s_Texure.CreateFromMemory(device, data.get(), size);
+}
+#endif // ARCHIVER_TEST
+
+#ifdef TEXTWRITE_TEST
+void TextWriteTest(ID3D11Device* device, ID3D11DeviceContext* context)
+{
+    s_Text.Create(TEXT("uzura.ttf"), TEXT("うずらフォント"), 24);
+    s_Text.WriteText(device, context, &s_Texure);
+}
+#endif // TEXTWRITE_TEST
 
 /**
  * create game object
@@ -14,12 +44,14 @@ Framework::Graphics::Render2D s_Render2D;
 bool Create(ID3D11Device* device, ID3D11DeviceContext* context)
 {
     s_Render2D.Create(device);
-    //s_Texure.CreateFromFile(device, TEXT("usa.png"));
 
-    s_Text.Create(TEXT("uzura.ttf"), TEXT("うずらフォント"), 24);
-
-    s_Text.WriteText(device, context, &s_Texure);
-
+#if defined ARCHIVER_TEST
+    ArchiverTest(device);
+#elif defined TEXTWRITE_TEST
+    TextWriteTest(device);
+#else
+    s_Texure.CreateFromFile(device, TEXT("usa.png"));
+#endif
 
     return true;
 }
