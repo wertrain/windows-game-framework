a647e3941f6043ad79554336a393beba4778901e
wertrain <wertrain@gmail.com>
2017-12-04 00:08:11 +0900

    Debug.cpp/h ファイル追加

---
+++ GameFramework/Framework/Common/Debug.cpp
@@ -0,0 +1,24 @@
+#include <tchar.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#include "Debug.h"
+
+void DebugPrintf(wchar_t* pszFormat, ...)
+{
+#ifdef _DEBUG
+#ifdef _WIN64
+
+    va_list	argp;
+    TCHAR pszBuf[256];
+    va_start(argp, pszFormat);
+
+    _vstprintf(pszBuf, sizeof(pszBuf), pszFormat, argp);
+    va_end(argp);
+    OutputDebugString(pszBuf);
+
+#endif // _WIN64
+#endif // DEBUG 
+}
+
+
---
+++ GameFramework/Framework/Common/Debug.h
@@ -0,0 +1,6 @@
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+void DebugPrintf(wchar_t* pszFormat, ...);
+
+#endif // DEBUG_H_
