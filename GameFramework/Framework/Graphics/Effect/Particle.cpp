3bad75b1fb46f8d941cf1499f1f21dbb7ba15656
wertrain <wertrain@gmail.com>
2018-01-13 22:12:35 +0900

    パーティクル関連のコード準備

---
+++ GameFramework/Framework/Graphics/Effect/Particle.cpp
@@ -0,0 +1,15 @@
+#include "Particle.h"
+
+NS_FW_GFX_BEGIN
+
+Particle::Particle()
+{
+
+}
+
+Particle::~Particle()
+{
+
+}
+
+NS_FW_GFX_END
---
+++ GameFramework/Framework/Graphics/Effect/Particle.h
@@ -0,0 +1,17 @@
+#ifndef GRAPHICS_EFFECT_PARTICLE_H_
+#define GRAPHICS_EFFECT_PARTICLE_H_
+
+#include "../../Common/Defines.h"
+
+NS_FW_GFX_BEGIN
+
+class Particle
+{
+public:
+    Particle() ;
+    ~Particle();
+};
+
+NS_FW_GFX_END
+
+#endif // GRAPHICS_EFFECT_PARTICLE_H_
