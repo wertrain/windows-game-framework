6ed021c85ef039b06b0dcaa4d2fefa976f7a4e7a
wertrain <wertrain@gmail.com>
2018-02-02 01:25:23 +0900

    インデント修正

--- GameFramework/Framework/Graphics/Model/ModelMqo.cpp
+++ GameFramework/Framework/Graphics/Model/ModelMqo.cpp
@@ -506,7 +506,7 @@ bool ModelMqo::Create(ID3D11Device* device, ID3D11DeviceContext* context, const 
     // 入力レイアウト定義
     D3D11_INPUT_ELEMENT_DESC layout[] = {
         { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,  0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        { "TEXCOORD", 0,    DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT,    0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
     };
     const UINT elem_num = ARRAYSIZE(layout);
 
