a7ca28f22737c2f2aac943a39c6da8c34488a5ff
wertrain <wertrain@gmail.com>
2018-04-29 13:45:41 +0900

    ログ関連ユーティリティファイルを追加

---
+++ Build/GameFramework/Include/nnfw/ut/Log.h
@@ -0,0 +1,14 @@
+/**
+ * @file Log.h
+ * @brief ログ関連ユーティリティ
+ */
+#ifndef UTILITY_LOG_H_
+#define UTILITY_LOG_H_
+
+#include <nnfw/common/Defines.h>
+
+NS_FW_UTIL_BEGIN
+
+NS_FW_UTIL_END
+
+#endif // UTILITY_LOG_H_
---
+++ Build/GameFramework/Source/nnfw/ut/Log.cpp
@@ -0,0 +1,5 @@
+/**
+ * @file Log.cpp
+ * @brief ログ関連ユーティリティ
+ */
+#include <nnfw/ut/Log.h>
