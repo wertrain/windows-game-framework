8b34dfce2030319af2e4a915e7c4cf690602bd1d
wertrain <wertrain@gmail.com>
2018-05-04 20:20:05 +0900

    ロケータークラスの追加

---
+++ Build/GameFramework/Include/nnfw/Locator.h
@@ -0,0 +1,18 @@
+/**
+ * @file Locator.h
+ * @brief ロケーター
+ */
+#ifndef NNFW_LOCATOR_H_
+#define NNFW_LOCATOR_H_
+
+NS_FW_BEGIN
+
+class Locator
+{
+private:
+    Locator();
+};
+
+NS_FW_END
+
+#endif
---
+++ Build/GameFramework/Source/nnfw/Locator.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file Locator.cpp
+ * @brief ロケーター
+ */
+#include <nnfw/common/Defines.h>
+
+#include <nnfw/Locator.h>
+
+NS_FW_BEGIN
+
+Locator::Locator()
+{
+
+}
+
+NS_FW_END
