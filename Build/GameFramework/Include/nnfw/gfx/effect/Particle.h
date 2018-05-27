a44244b78825c4ab7dc1e372d60a884bd048dce6
wertrain <wertrain@gmail.com>
2018-05-28 00:06:51 +0900

    パーティクルクラス実装（仮）

--- Build/GameFramework/Include/nnfw/gfx/effect/Particle.h
+++ Build/GameFramework/Include/nnfw/gfx/effect/Particle.h
@@ -5,18 +5,48 @@
 #ifndef GRAPHICS_EFFECT_PARTICLE_H_
 #define GRAPHICS_EFFECT_PARTICLE_H_
 
-#include <nnfw/common/Defines.h>
+#include <nnfw/ut/CircularArray.h>
+#include <nnfw/gfx/GraphicsBase.h>
 
 NS_FW_GFX_BEGIN
 
 /// <summary>
 /// パーティクルを表すクラス
 /// </summary>
-class Particle
+class Particles
 {
 public:
-    Particle() ;
-    ~Particle();
+    Particles();
+    ~Particles();
+
+    bool Create(ID3D11Device* device, ID3D11DeviceContext* context, const uint32_t instanceNum);
+    void Destroy();
+    void Render(ID3D11DeviceContext* context);
+
+private:
+    struct Particle
+    {
+        Vector4 pos;
+    };
+
+private:
+    ID3D11InputLayout* mVertexLayout;
+    ID3D11Buffer* mVertexBuffer;
+    ID3D11Buffer* mInstancingVertexBuffer;
+    ID3D11Buffer* mIndexBuffer;
+
+    ID3D11BlendState* mBdState;
+
+    ID3D11VertexShader* mVertexShader;
+    ID3D11PixelShader* mPixelShader;
+    uint32_t mVertexNum;
+    uint32_t mVertexDataSize;
+
+    ID3D11Resource* mTexture;
+    ID3D11ShaderResourceView* mShaderResView;
+    ID3D11SamplerState* mSampler;
+    uint32_t mInstanceNum;
+    NS_FW_UTIL::CircularArray<Particle> mParticles;
 };
 
 NS_FW_GFX_END
