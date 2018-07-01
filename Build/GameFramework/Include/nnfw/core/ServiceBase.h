7c3a813ca05339340cc84dd637248498b0300208
wertrain <wertrain@gmail.com>
2018-07-01 21:24:44 +0900

    サービスの基底クラスの追加

---
+++ Build/GameFramework/Include/nnfw/core/ServiceBase.h
@@ -0,0 +1,22 @@
+/**
+ * @file ServiceBase.h
+ * @brief サービスの基底クラス
+ */
+#ifndef CORE_SERVICEBASE_H_
+#define CORE_SERVICEBASE_H_
+
+NS_FW_SYS_BEGIN
+
+/// <summary>
+/// サービスの基底クラス
+/// </summary>
+class ServiceBase
+{
+public:
+    ServiceBase();
+    ~ServiceBase();
+};
+
+NS_FW_SYS_END
+
+#endif // CORE_SERVICEBASE_H_
---
+++ Build/GameFramework/Source/nnfw/core/ServiceBase.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file ServiceBase.cpp
+ * @brief サービスの基底クラス
+ */
+#include "Precompiled.h"
+
+#include <nnfw/core/ServiceBase.h>
+
+NS_FW_SYS_BEGIN
+
+ServiceBase::ServiceBase()
+{
+
+}
+
+ServiceBase::~ServiceBase()
+{
+
+}
+
+NS_FW_SYS_END
