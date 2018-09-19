4b5830d48e8945e9fa13d67003a8f2f370efc15d
wertrain <wertrain@gmail.com>
2018-09-20 00:01:00 +0900

    ログのタイプを追加

--- Build/GameFramework/Include/nnfw/ut/Log.h
+++ Build/GameFramework/Include/nnfw/ut/Log.h
@@ -12,6 +12,17 @@ NS_FW_UTIL_BEGIN
 class Log
 {
 public:
+    enum Type : uint8_t
+    {
+        TYPE_BEGIN   = 0,
+        TYPE_SYSTEM  = 1 << 0,
+        TYPE_INFO    = 1 << 1,
+        TYPE_CAUTION = 1 << 2,
+        TYPE_DANGER  = 1 << 3,
+        TYPE_END
+    };
+
+public:
     Log();
     ~Log();
 };
