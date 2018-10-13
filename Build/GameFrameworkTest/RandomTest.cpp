a70f3f1d92ab1d49c8f75f95c874787e575df846
wertrain <wertrain@gmail.com>
2018-10-14 00:00:00 +0900

    WIP

--- Build/GameFrameworkTest/RandomTest.cpp
+++ Build/GameFrameworkTest/RandomTest.cpp
@@ -20,19 +20,19 @@ namespace GameFrameworkTest
             char buffer[256];
 
             fw::util::Random random((unsigned long)time(nullptr));
-            for (int i = 0; i < 10; ++i)
+            for (int i = 0; i < 16; ++i)
             {
                 float f = random.NextFloat();
                 sprintf_s(buffer, "Float %f\n", f);
                 Logger::WriteMessage(buffer);
             }
-            for (int i = 0; i < 10; ++i)
+            for (int i = 0; i < 16; ++i)
             {
                 uint32_t f = random.NextUInt();
                 sprintf_s(buffer, "Uint %u\n", f);
                 Logger::WriteMessage(buffer);
             }
-            for (int i = 0; i < 10; ++i)
+            for (int i = 0; i < 16; ++i)
             {
                 uint32_t f = random.GetRange(0, 10);
                 sprintf_s(buffer, "Uint %u\n", f);
